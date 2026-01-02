2d612e67b603ae72a9760011b3344d011311ab22
Serein-21 <166235982+Serein-21@users.noreply.github.com>
2026-01-02 12:18:31 +0530

    Create 204-CountPrimes

---
+++ 204-CountPrimes/204-CountPrimes.cpp
@@ -0,0 +1,25 @@
+// Last updated: 1/2/2026, 12:18:31 PM
+class Solution {
+public:
+    int countPrimes(int n) {
+        int cnt=0;
+        vector<bool>prime(n+1,true);
+        prime[0]=prime[1]=false;
+        for (int i=2;i<n;i++)
+        {
+            if (prime[i])
+            {
+              cnt++;
+
+              for (int j=2*i;j<n;j=j+i)
+              {
+                prime[j]=0;
+              }
+            }
+        }
+
+        return cnt;
+
+    }
+    
+};
